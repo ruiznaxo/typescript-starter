bbc554b0df44e3fc8b60e8752741f94ae066a8a9
Ignacio <ruiznaxo@gmail.com>
2021-03-26 17:22:34 -0300

    primer commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import { AuthenticationModule } from './authentication/authentication.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthenticationModule,
+    UsersModule, PostsModule, ConfigModule.forRoot(
+      {
+        validationSchema: Joi.object({
+          POSTGRES_HOST: Joi.string().required(),
+          POSTGRES_PORT: Joi.number().required(),
+          POSTGRES_USER: Joi.string().required(),
+          POSTGRES_PASSWORD: Joi.string().required(),
+          POSTGRES_DB: Joi.string().required(),
+          PORT: Joi.number(),
+          JWT_SECRET: Joi.string().required(),
+          JWT_EXPIRATION_TIME: Joi.string().required(),
+        })
+      }
+    ), DatabaseModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
